c52cb6503d73e06e6c7d586a242c8633dacc31fc
Troyansky1 <troyansky1@mail.tau.ac.il>
2024-02-15 13:51:18 +0200

    Added basic tests

---
+++ avl_test.py
@@ -0,0 +1,42 @@
+
+import random
+
+import avl_template
+
+
+def print_avl_tree(root, prefix="", is_left=True):
+    if root is not None:
+        print(prefix + ("|-- " if is_left else "\\-- ") +"KEY:" + str(root.key) + " BF:" + str(root.get_BF()))
+        print_avl_tree(root.left, prefix + ("|   " if is_left else "    "), True)
+        print_avl_tree(root.right, prefix + ("|   " if is_left else "    "), False)
+
+
+def test_avl_template():
+    def test1():
+        for i in range(10):
+            avl.insert(i, i)
+        print_avl_tree(avl.get_root())
+
+    def test2():
+        nums_in_tree = []
+        for i in range(50):
+            num = random.randint(0, 100)
+            nums_in_tree.append(num)
+            if num not in nums_in_tree:
+                avl.insert(num, num)
+        print_avl_tree(avl.get_root())
+
+        for i in range(10):
+            num = random.choice(nums_in_tree)
+            avl.delete(num)
+        print_avl_tree(avl.get_root())
+
+
+    avl = avl_template.AVLTree()
+    test1()
+
+if __name__ == '__main__':
+    test_avl_template()
+
+
+
