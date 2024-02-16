dcab7f189bae26551f6384bb0616c44904bd4868
Troyansky1 <troyansky1@mail.tau.ac.il>
2024-02-16 16:23:35 +0200

    Added more tests

--- avl_test.py
+++ avl_test.py
@@ -1,54 +1,92 @@
 
 import random
-
+import inspect
 import avl_template
 
 
-def print_avl_tree(root, prefix="", is_left=True):
+def print_avl_tree(root, title= "Tree"):
+    print(title)
+    print_avl_tree_req(root)
+    print("\n")
+
+def print_avl_tree_req(root, prefix="", is_left=True):
     if root is not None:
         key_str = "KEY:" + str(root.key)
         bf_str = " BF:" + str(root.get_BF())
         height_str = " Height:" + str(root.get_height())
 
         print(prefix + ("|-- " if is_left else "\\-- ") + key_str + bf_str + height_str)
-        print_avl_tree(root.left, prefix + ("|   " if is_left else "    "), True)
-        print_avl_tree(root.right, prefix + ("|   " if is_left else "    "), False)
+        print_avl_tree_req(root.left, prefix + ("|   " if is_left else "    "), True)
+        print_avl_tree_req(root.right, prefix + ("|   " if is_left else "    "), False)
 
 
+def print_test(test):
+    print("*"*40)
+    print("_"*16, test, "_"*16)
+    print("*" * 40)
 
-def test_avl_template():
-    def test1():
-        for i in range(10):
-            avl.insert(i, i)
-
-        print_avl_tree(avl.get_root())
-        print("\n")
-        node = avl.search(7)
-        avl.delete(node)
-        print_avl_tree(avl.get_root())
-
-    def test2():
-        nums_in_tree = []
-        for i in range(50):
-            num = random.randint(0, 100)
-            if num not in nums_in_tree:
-                avl.insert(num, num)
-            nums_in_tree.append(num)
-        print_avl_tree(avl.get_root())
-        print("\n")
-
-        for i in range(10):
-            num = random.choice(nums_in_tree)
-            avl.delete(num)
-        print_avl_tree(avl.get_root())
-
-
+#Insert in order and a few deletes. uses rotations.
+def test1():
+    print_test(inspect.currentframe().f_code.co_name)
     avl = avl_template.AVLTree()
-    test1()
-    #test2()
+    for i in range(30):
+        avl.insert(i, i)
+    node = avl.search(7)
+    avl.delete(node)
+    node = avl.search(9)
+    avl.delete(node)
+    print_avl_tree(avl.get_root())
+
+# Insert in reverse order and a few deletes. uses rotations.
+def test2():
+    print_test(inspect.currentframe().f_code.co_name)
+    avl = avl_template.AVLTree()
+    for i in range(29, 0, -1):
+        avl.insert(i, i)
+    node = avl.search(7)
+    avl.delete(node)
+    node = avl.search(9)
+    avl.delete(node)
+    print_avl_tree(avl.get_root())
+
+#insert random numbers.
+def test3():
+    print_test(inspect.currentframe().f_code.co_name)
+    avl = avl_template.AVLTree()
+    nums_in_tree = []
+    for i in range(20):
+        num = random.randint(0, 100)
+        if num not in nums_in_tree:
+            avl.insert(num, num)
+            print_avl_tree(avl.get_root())
+        nums_in_tree.append(num)
+    print_avl_tree(avl.get_root())
+
+#inser random numbers, than delete random numbers.
+def test4():
+    print_test(inspect.currentframe().f_code.co_name)
+    avl = avl_template.AVLTree()
+    nums_in_tree = []
+    for i in range(50):
+        num = random.randint(0, 100)
+        if num not in nums_in_tree:
+            avl.insert(num, num)
+        nums_in_tree.append(num)
+    print_avl_tree(avl.get_root())
+
+    for i in range(50):
+        num = random.randint(0, 100)
+        node = avl.search(num)
+        if node is not None:
+            avl.delete(node)
+    print_avl_tree(avl.get_root())
+
+
 
 if __name__ == '__main__':
-    test_avl_template()
-
+    #test1()
+    #test2()
+    #test3()
+    test4()
 
 
