9fc67a4f8cc87801e190b4b3457db8457e5831bc
Troyansky1 <troyansky1@mail.tau.ac.il>
2024-02-22 22:09:44 +0200

    Fixed timing test

--- timing_tests.py
+++ timing_tests.py
@@ -31,40 +31,45 @@ def get_rand_key_not_in_tree(tree, max):
     return key
 
 
-def calc_rand_cost(tree, tree_size):
+def calc_rand_node_cost(tree, tree_size):
     rand_key = get_rand_node_in_tree(tree, tree_size)
-    rand_cost = tree.split(rand_key)[2] #TODO change in avl tree, return cost from split
-    return rand_cost
+    t1, t2, avg_cost, max_cost = tree.split(rand_key)
+    return avg_cost, max_cost
 
 
-def calc_max_cost(tree):
+def calc_max_node_cost(tree):
     max_key = get_max_node(tree.get_root().get_left())
-    max_cost = tree.split(max_key)[3] #TODO change in avl tree return cost from split
-    return max_cost
+    t1, t2, avg_cost, max_cost = tree.split(max_key)
+    return avg_cost, max_cost
 
 def get_max_node(node):
     while node.get_right() is not None and node.get_right().is_real_node():
         node = node.get_right()
     return node
 def run_and_calc_cost(num_of_runs, i):
-    max_cost_rand = 0
-    sum_costs_rand = 0
-    max_cost_max = 0
-    sum_costs_max = 0
+    sum_costs_rand_node_max = 0
+    sum_costs_rand_node_avg = 0
+    sum_costs_max_node_max = 0
+    sum_costs_max_node_avg = 0
+
     for j in range(num_of_runs):
         tree1, tree2 = build_random_tree(i)
-        cost_rand = calc_rand_cost(tree1, get_tree_size(i))
-        if cost_rand > max_cost_rand:
-            max_cost_rand = cost_rand
-        sum_costs_rand += cost_rand
 
-        cost_max = calc_max_cost(tree2)
-        if cost_max > max_cost_max:
-            max_cost_max = cost_max
-        sum_costs_max += cost_max
-    avg_cost_rand = sum_costs_rand/num_of_runs
-    avg_cost_max = sum_costs_max/num_of_runs
-    return max_cost_rand, avg_cost_rand, max_cost_max, avg_cost_max
+        # Tree 1
+        avg_cost_rand_node, max_cost_rand_node = calc_rand_node_cost(tree1, get_tree_size(i))
+        sum_costs_rand_node_max += max_cost_rand_node
+        sum_costs_rand_node_avg += avg_cost_rand_node
+
+        # Tree 2
+        avg_cost_max_node, max_cost_max_node = calc_max_node_cost(tree2)
+        sum_costs_max_node_max += max_cost_max_node
+        sum_costs_max_node_avg += avg_cost_max_node
+
+    avg_cost_rand_node_max = sum_costs_rand_node_max / num_of_runs
+    avg_cost_rand_node_avg = sum_costs_rand_node_avg / num_of_runs
+    avg_cost_max_node_max = sum_costs_max_node_max / num_of_runs
+    avg_cost_max_node_avg = sum_costs_max_node_avg / num_of_runs
+    return avg_cost_rand_node_max, avg_cost_rand_node_avg, avg_cost_max_node_max, avg_cost_max_node_avg
 
 
 def run_increment_i(max_i):
@@ -79,4 +84,4 @@ def run_increment_i(max_i):
         print("Avg cost for spliting from the max node is", avg_cost_max)
 
 
-run_increment_i(8)
+run_increment_i(10)
